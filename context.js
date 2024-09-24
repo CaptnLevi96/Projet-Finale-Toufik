dfbe7003d41e3fe24d5770ed6f729ac19e7bbfef
Lnx <46533182+lnxAv@users.noreply.github.com>
2024-09-24 15:53:23 -0400

    Context pour lapplication

---
+++ context.js
@@ -0,0 +1,26 @@
+class Context {
+  constructor(currency, sign, exchangeRate, country) {
+    this.currency = currency;
+    this.sign = sign;
+    this.exchangeRate = exchangeRate;
+    this.country = country;
+  }
+
+  setCurrency(currency) {
+    this.currency = currency;
+  }
+
+  setSign(sign) {
+    this.sign = sign;
+  }
+
+  setExchangeRate(exchangeRate) {
+    this.exchangeRate = exchangeRate;
+  }
+
+  setCountry(country) {
+    this.country = country;
+  }
+}
+
+// const _context = new Context("CAD", "$", 1.3, "CA");
